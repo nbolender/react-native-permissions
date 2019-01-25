1f79b3719496c089e20abf7c0db9157780c626fc
Nathan Bolender <nathan@nathanbolender.com>
2019-01-25 00:23:27 -0500

    Notifications only

--- ios/RCTConvert+RNPStatus.h
+++ ios/RCTConvert+RNPStatus.h
@@ -22,19 +22,19 @@ static NSString* RNPStatusRestricted = @"restricted";
 
 typedef NS_ENUM(NSInteger, RNPType) {
     RNPTypeUnknown,
-    RNPTypeLocation,
-    RNPTypeCamera,
-    RNPTypeMicrophone,
-    RNPTypePhoto,
-    RNPTypeContacts,
-    RNPTypeEvent,
-    RNPTypeReminder,
-    RNPTypeBluetooth,
+//    RNPTypeLocation,
+//    RNPTypeCamera,
+//    RNPTypeMicrophone,
+//    RNPTypePhoto,
+//    RNPTypeContacts,
+//    RNPTypeEvent,
+//    RNPTypeReminder,
+//    RNPTypeBluetooth,
     RNPTypeNotification,
-    RNPTypeBackgroundRefresh,
-    RNPTypeSpeechRecognition,
-    RNPTypeMediaLibrary,
-    RNPTypeMotion
+//    RNPTypeBackgroundRefresh,
+//    RNPTypeSpeechRecognition,
+//    RNPTypeMediaLibrary,
+//    RNPTypeMotion
 };
 
 @interface RCTConvert (RNPStatus)
--- ios/RCTConvert+RNPStatus.m
+++ ios/RCTConvert+RNPStatus.m
@@ -10,19 +10,19 @@
 
 @implementation RCTConvert (RNPStatus)
 
-RCT_ENUM_CONVERTER(RNPType, (@{ @"location" : @(RNPTypeLocation),
-                                @"camera" : @(RNPTypeCamera),
-                                @"microphone" : @(RNPTypeMicrophone),
-                                @"photo" : @(RNPTypePhoto),
-                                @"contacts" : @(RNPTypeContacts),
-                                @"event" : @(RNPTypeEvent),
-                                @"reminder" : @(RNPTypeReminder),
-                                @"bluetooth" : @(RNPTypeBluetooth),
-                                @"notification" : @(RNPTypeNotification),
-                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh),
-                                @"speechRecognition": @(RNPTypeSpeechRecognition),
-                                @"mediaLibrary": @(RNPTypeMediaLibrary),
-                                @"motion": @(RNPTypeMotion)
+RCT_ENUM_CONVERTER(RNPType, (@{ //@"location" : @(RNPTypeLocation),
+//                                @"camera" : @(RNPTypeCamera),
+//                                @"microphone" : @(RNPTypeMicrophone),
+//                                @"photo" : @(RNPTypePhoto),
+//                                @"contacts" : @(RNPTypeContacts),
+//                                @"event" : @(RNPTypeEvent),
+//                                @"reminder" : @(RNPTypeReminder),
+//                                @"bluetooth" : @(RNPTypeBluetooth),
+                                @"notification" : @(RNPTypeNotification)//,
+//                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh),
+//                                @"speechRecognition": @(RNPTypeSpeechRecognition),
+//                                @"mediaLibrary": @(RNPTypeMediaLibrary),
+//                                @"motion": @(RNPTypeMotion)
                                 }),
                                 RNPTypeUnknown, integerValue)
 
